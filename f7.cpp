f66eae05e6efc6f534dd7ff8f225f418fbcd761b
Viraj Solanki <virajsolanki2519@gmail.com>
2024-03-22 22:20:54 -0400

    binary file is created and all the comments are added

--- f7.cpp
+++ f7.cpp
@@ -1,32 +1,48 @@
-// f7.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+* FILE: f7.cpp
+* PROJECT: SENG1000 - Focused Assignment 7
+* PROGRAMMER: Virajsinh Dharmendrasinh Solanki
+* FIRST VERSION: 2024-03-21
+* DESCRIPTION: This program is used to create two files 
+*              one text file and one binary file and it 
+*              will write inside it according to the requirements.  
+*/
 
 #include <stdio.h>
-#pragma warning(disable: 4996) //
+#pragma warning(disable: 4996) 
 int main(void)
 {
     const unsigned short kBinaryData[] = { 26946, 24942, 31090,
     25632, 29793, 8289, 28518, 8306, 28537, 33141, 39308 };
-    unsigned short binary;
     FILE* filePointer;
     FILE* binaryPointer;
-    // Open the file for reading and appending. 
-    // If it doesn't exist, create it.
+    // open the file through the pointer
     filePointer = fopen("myTextFile.txt", "w");
-  
 
     // Write data to the file using fprintf
     fprintf(filePointer, "%s %s %s %d\n", "This", "is", "line", 1);
     fprintf(filePointer, "%s %s %s %d\n", "This", "is", "line", 2);
 
-    binaryPointer = fopen("myEvenDataList.data", "wb");
-    for (int i = 0; i < sizeof(kBinaryData)/ sizeof(kBinaryData[0]); i++) {
-        
-        if(kBinaryData[i]%2==0){
-            fwrite(&kBinaryData[i],sizeof(unsigned short) , 1, binaryPointer);
-        }
+    if (filePointer == NULL) {
+        //if the file is not created then it will show this error
+        printf("error text file named myTextFile file is not created");
+        return 0;
     }
-
-  
+    fclose(filePointer);
+    binaryPointer = fopen("myEvenDataList.data", "wb");
+   // opens the file through the pointer
+   
+    for (int i = 0; i < sizeof(kBinaryData) / sizeof(kBinaryData[0]); i++) {
+        if (kBinaryData[i] % 2 == 0) {
+            // this will write all the even numbers in the data file
+            fwrite(&kBinaryData[i], sizeof(unsigned short), 1, binaryPointer);
+        }   
+    }
     
+    if (binaryPointer == NULL) {
+        printf("error binary file named myEvenDataList.data file is not created");
+        //if the file is not created then it will show this error
+        return 0;
+    }
+    fclose(binaryPointer);
 }
