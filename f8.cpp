43ccdd958818b1e05c3837ab4c1d7fdc28eb29ad
krishna140404 <Krishnanachhiapatel14@gmail.com>
2024-03-24 18:12:49 -0400

    the structure and arguments in the code are completed file creation is left

---
+++ f8.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#pragma warning(disable:4996) 
+
+struct MyData {
+	int howMany;
+	char theText[21];
+	char directoryPath[21];
+	char filename[21];
+};
+int main(int argc, char** argv) {
+	if (argc != 5) {
+		printf("invalid amount of arguments");
+		return 0;
+	}
+	struct MyData myArgs;
+	myArgs.howMany = atoi(argv[1]);
+	strncpy(myArgs.theText, argv[2], sizeof(myArgs.theText) - 1);
+	strncpy(myArgs.directoryPath, argv[3], sizeof(myArgs.directoryPath) - 1);
+	strncpy(myArgs.filename, argv[4], sizeof(myArgs.filename) - 1);
+
+}
+
